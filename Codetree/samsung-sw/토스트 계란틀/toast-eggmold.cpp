f516adc7260ea9d56976290a38c2bfcd557b7722
KIM HYEON SEOK <136921645+hyeonseokKI@users.noreply.github.com>
2026-09-15 12:20:28 +0900

    [Passed] 토스트 계란틀 (6ms, 0MB)

--- Codetree/samsung-sw/토스트 계란틀/toast-eggmold.cpp
+++ Codetree/samsung-sw/토스트 계란틀/toast-eggmold.cpp
@@ -13,14 +13,13 @@ const int MX = 50 + 2;
 
 int n,l,r;
 int board[MX][MX];
-int dist[MX][MX];   // -1 : 방문 x , 
+bool vis[MX][MX];
 
-int dx[4]= {1,0,-1,0};
-int dy[4]= {0,1,0,-1};
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
 
-bool is_moved = true;
+bool is_moved;
 int ans;
-
 void print_board(int arr[MX][MX]);
 
 int main(void){
@@ -29,83 +28,82 @@ int main(void){
     
 
     cin >> n >> l >> r;
+
     for(int i = 0 ; i < n ; i ++ ){
-        for(int j = 0 ; j < n ; j ++){
+        for(int j = 0 ; j < n ; j ++ ){
             cin >> board[i][j];
         }
     }
 
-    // print_board(board);  // debug
 
 
-    while(1){
+    while(true){
         is_moved = false;
+        for(int i = 0 ; i < n; i ++) fill(vis[i],vis[i]+n,0);
         
-        int idx = 1;
-
-        for(int i = 0 ; i < n ; i++ ) fill(dist[i],dist[i]+MX,-1);
-        vector<pair<int,int>> g_num[MX*MX];
-        for(int i = 0 ; i < n ; i ++ ){
-            for(int j = 0 ; j < n ; j ++){
-                if(dist[i][j] != -1) continue;  // 방문했다면 패스
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                queue<pair<int,int>> q;
+                vector<pair<int,int>> cand;
+                int sum = 0;
+                if(vis[i][j]) continue;
                 
-                queue<pair<int,int>> q; 
                 q.push({i,j});
-                g_num[idx].push_back({i,j});
-                dist[i][j] = idx++;
+                vis[i][j]=1;
+                sum += board[i][j];
+                cand.push_back({i,j});
+
                 while(!q.empty()){
                     auto cur = q.front(); q.pop();
-                    for(int dir = 0 ; dir < 4 ; dir++){
-                        int nx = cur.X + dx[dir] ;
+                    for(int dir = 0 ; dir <4 ; dir++){
+                        int nx = cur.X + dx[dir];
                         int ny = cur.Y + dy[dir];
-                        if(nx < 0 || ny < 0 || nx >= n || ny >= n ) continue;
-                        if(dist[nx][ny] != -1) continue;
-                        if(abs(board[cur.X][cur.Y] - board[nx][ny]) < l ) continue;
-                        if(abs(board[cur.X][cur.Y] - board[nx][ny]) > r ) continue;
-                        q.push({nx,ny});                        
-                        dist[nx][ny] = dist[cur.X][cur.Y];
-                        g_num[dist[cur.X][cur.Y]].push_back({nx,ny});
-                        is_moved = true;
+                        if(nx < 0 || ny < 0 || nx >= n || ny >= n )continue;
+                        if(vis[nx][ny]) continue;
+                        int diff = abs(board[cur.X][cur.Y] - board[nx][ny]);
+                        if(diff < l || diff > r) continue;
+                        q.push({nx,ny});
+                        vis[nx][ny] = 1;
+                        sum += board[nx][ny];
+                        cand.push_back({nx,ny});
                     }
+                    
                 }
 
+                int tot = sum / (int)cand.size();
+                for(auto& c : cand){
+                    board[c.X][c.Y] = tot;                 
+                }
+                if(cand.size() > 1)  is_moved =true;
             }
         }
 
-        // 계란 옮기기 , 소숫점 버림
 
-        for(int i = 1; i < idx; i ++){
-            int tmp = 0 ;
-            
-            for(auto &v : g_num[i]){
-                tmp += board[v.X][v.Y];
-            }
-            tmp /= g_num[i].size();
-
-            for(auto &v : g_num[i]){
-                board[v.X][v.Y] = tmp;
-            }
-            // cout << tmp  << '\n';
-
-
-        }
         // print_board(board);
-        // break;
+
         if(!is_moved) break;
         ans++;
     }
+    
 
 
-    cout << ans ;
+
+
+    cout << ans;
     
 }
 
 
 void print_board(int arr[MX][MX]){
-    for(int i = 0 ; i < n ; i ++ ){
-        for(int j = 0 ; j < n ; j ++){
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
             cout << arr[i][j] << ' ';
         }
         cout << '\n';
     }
+
+    cout << '\n';
+
+
 }
